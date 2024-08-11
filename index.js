e40a34f55ce39be9185d748f1d115bd87c773796
Mahbubur Rahman <mahbub2003814@gmail.com>
2024-08-11 23:52:09 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let navbar = document.querySelector(".nav");
+navbar.style.maxHeight == "0px";
+
+function togglemenu(){
+    if(navbar.style.maxHeight == "0px"){
+        navbar.style.maxHeight = "600px";
+    }
+    else{
+        navbar.style.maxHeight = "0px"
+    }
+}
